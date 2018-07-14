9fc8c6d8fd3a43250f2eec767004e28b007a91c0
Emily Jirles <emily.jirles@gmail.com>
2018-07-14 22:16:20 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -1,6 +1,27 @@
-const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
-
 function init() {
-  // Write your JavaScript code inside the init() function
+// add event listener 
+  let closedEventFunction = setKonamiEasterEgg();
 
-}
+  document.body.addEventListener('keydown', closedEventFunction);
+};
+
+// set up closure for easter egg function
+function setKonamiEasterEgg() {
+  const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65];
+  let index = 0;
+  return function(e){
+    const key = parseInt(e.which);
+
+    if (key === code[index]) {
+      index++;
+
+      if (index === code.length) {
+        alert("Hurray!");
+
+        index = 0;
+      }
+    } else {
+      index = 0;
+    }
+  };
+};
